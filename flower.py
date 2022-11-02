cc2ecefd6277173f4065f86174cd453e03c460ab
r1khil <rikhil.sharma@outlook.com>
2022-11-02 13:06:18 -0400

    Update flower.py

--- flower.py
+++ flower.py
@@ -29,6 +29,51 @@ def drawpetal():
 
   painter.end_fill()
 
+
+def draw_petals():
+
+  #draws petal
+
+  for outside in range(6):
+    drawpetal()
+    painter.left(30)
+
+
+def yellow_hexagon():
+ 
+  #switches to yellow
+  painter.pencolor("yellow")
+  painter.fillcolor("yellow")
+
+  #prepares for inner yellow
+  painter.setheading(0)
+  painter.pensize(10)
+
+  #draws inner hexagon
+  painter.begin_fill()
+  for hexagon in range(6):
+    painter.forward(100)
+    painter.left(300)
+  painter.end_fill()
+
+def black_circle():
+ 
+  #prepares for black circle
+
+  painter.pencolor("black")
+  painter.fillcolor("black")
+  painter.penup()
+  painter.goto(50,-25)
+
+
+  #black circle
+
+  painter.begin_fill()
+  painter.circle(40) 
+  painter.end_fill()
+  painter.hideturtle()
+
+
 #sets color blue
 painter.pencolor("blue")
 painter.fillcolor("blue")
@@ -43,42 +88,15 @@ painter.goto(0,100)
 painter.setheading(90)
 painter.pendown()
 
-#draws petal
-
-for outside in range(6):
-  drawpetal()
-  painter.left(30)
-  
-#switches to yellow
-painter.pencolor("yellow")
-painter.fillcolor("yellow")
-
-#prepares for inner yellow
-painter.setheading(0)
-painter.pensize(10)
-
-#draws inner hexagon
-painter.begin_fill()
-for hexagon in range(6):
-  painter.forward(100)
-  painter.left(300)
-painter.end_fill()
-
- 
-#prepares for black circle
-
-painter.pencolor("black")
-painter.fillcolor("black")
-painter.penup()
-painter.goto(50,-25)
+#execution
 
 
-#blackc circle
+draw_petals()
+yellow_hexagon()
+black_circle()
 
-painter.begin_fill()
-painter.circle(40) 
-painter.end_fill()
-painter.hideturtle()
+
+
 
 
 wn = turtle.Screen()
