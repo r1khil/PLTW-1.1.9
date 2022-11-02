5847d58e0184058aa3695403415401d8e9eefa73
r1khil <rikhil.sharma@outlook.com>
2022-11-02 12:54:03 -0400

    finished flower - need to make it more complicated

--- flower.py
+++ flower.py
@@ -29,14 +29,57 @@ def drawpetal():
 
   painter.end_fill()
 
+#sets color blue
+painter.pencolor("blue")
+painter.fillcolor("blue")
+
+# sets up the painter
 painter.penup()
 painter.goto(0,100)
 
+
+
+
 painter.setheading(90)
 painter.pendown()
+
+#draws petal
+
 for outside in range(6):
   drawpetal()
   painter.left(30)
-    
+  
+#switches to yellow
+painter.pencolor("yellow")
+painter.fillcolor("yellow")
+
+#prepares for inner yellow
+painter.setheading(0)
+painter.pensize(10)
+
+#draws inner hexagon
+painter.begin_fill()
+for hexagon in range(6):
+  painter.forward(100)
+  painter.left(300)
+painter.end_fill()
+
+ 
+#prepares for black circle
+
+painter.pencolor("black")
+painter.fillcolor("black")
+painter.penup()
+painter.goto(50,-25)
+
+
+#blackc circle
+
+painter.begin_fill()
+painter.circle(40) 
+painter.end_fill()
+painter.hideturtle()
+
+
 wn = turtle.Screen()
 wn.mainloop()
